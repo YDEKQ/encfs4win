dd0c6c914bafa983f2194565b7883c9ca62a965a
Charles Munson <jetwhiz@jetwhiz.com>
2020-01-26 20:52:16 -0500

    Bump versions
    Dokany and OpenSSL updates
    Prepare for 1.10.2 release

--- build-openssl.bat
+++ build-openssl.bat
@@ -23,8 +23,8 @@ REM along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
 REM versioning variables 
-set VERSION=1.0.2t
-set VERSION_STR=1_0_2t
+set VERSION=1.0.2u
+set VERSION_STR=1_0_2u
 set SOURCE_URI=https://github.com/openssl/openssl.git
 
 
